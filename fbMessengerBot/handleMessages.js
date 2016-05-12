1857b35d452d0b70664815901ec8205986cb0b42
Harijs Deksnis <harijs.deksnis@gmail.com>
2016-05-12 16:53:05 +0200

    assign different variable

--- fbMessengerBot/handleMessages.js
+++ fbMessengerBot/handleMessages.js
@@ -7,16 +7,18 @@ var debugMode = require('./config/debugMode');
 
 module.exports = function (senderId, message) {
     
+    var messageText = message.text;
+
     //if (debugMode.getDebugMode(senderId)) {
         var textReply = new fbMessage
-            .PlainText("[DEBUG] Sender:Id" + senderId + " Message JSON: " + JSON.stringify(message))
+            .PlainText("[DEBUG] SenderId: " + senderId + " Message JSON: " + JSON.stringify(message))
             .compose();
 
         sendMessage(senderId, textReply);
     //}
 
 
-    if (message.text.toLowerCase().substr(0, 5) === '@bot ') {
+    if (messageText.toLowerCase().substr(0, 5) === '@bot ') {
 
 
         sendMessage(senderId, {
@@ -25,7 +27,7 @@ module.exports = function (senderId, message) {
 
         handleBotCommands({
             senderId : senderId,
-            command : message.text.substr(5)
+            command : messageText.toLowerCase().substr(5)
         });
 
     }
